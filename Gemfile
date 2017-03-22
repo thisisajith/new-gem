f83fb48cebf1dc5c6b10d9de152bbcb9d403ed13
ajith <ajith@codingmart.com>
2017-03-22 17:14:56 +0530

    first gem

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in First_gem.gemspec
+gemspec
---
+++ lib/First_gem.rb
@@ -0,0 +1,11 @@
+require "First_gem/version"
+
+module FirstGem
+ class name
+ def self.hi
+ 	puts "hi...."
+ end
+ def self.hi(name)
+ 	puts "hiii...#{name}"
+ end
+end
